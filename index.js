ce37786ea70ecd7284e78620eba6f4fbccfe3104
MaxFray8908 <nadja.gawrilowa@yandex.ru>
2023-04-20 21:18:02 +0400

    feat: add stylization

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const keyboardEnglish = [['`','1','2','3','4','5','6','7','8','9','0','-','=','Backspace'],
                         ['Tab','q','w','e','r','t','y','u','i','o','p','[',']','\\', 'Del'],
                         ['CapsLock','a','s','d','f','g','h','j','k','l',';','\'','Enter'],
-                        ['Shift','z','x','c','v','b','n','m',',','.','/','В','Shift'],
-                        ['Ctrl','Win','Alt','','Alt','л','н','п','ctrl']];
+                        ['Shift','z','x','c','v','b','n','m',',','.','/','Shift'],
+                        ['Ctrl','Win','Alt','','Alt','◀','▲','▼','▶','ctrl']];
 const keyboardRussian = [['ё','1','2','3','4','5','6','7','8','9','0','-','=','Backspace'],
                         ['Tab','й','ц','у','к','е','н','г','ш','щ','з','х','ъ','\\', 'Del'],
                         ['CapsLock','ф','ы','в','а','п','р','о','л','д','ж','э','Enter'],
@@ -26,8 +26,15 @@ function addKeyboard(language) {
     wrapperKeyboard.insertAdjacentHTML("beforeend", `<div class="keyboard-row"></div>`);
     let keyboardRow = document.querySelectorAll(".keyboard-row")[i];
     for (let j = 0; j < language[i].length; j++) {
-      if (language[i][j].length === 1) {
+      if (language[i][j].length === 1 && language[i][j] !== '▲') {
         keyboardRow.insertAdjacentHTML("beforeend", `<button class="button button_short">${language[i][j]}</button>`);
+      }
+      else if (language[i][j] === '▲') {
+        keyboardRow.insertAdjacentHTML("beforeend", `<div class="wrapper-button_arrows"> 
+                                                        <button class="button button_arrows">${language[i][j]}</button> 
+                                                        <button class="button button_arrows">${language[i][j+1]}</button> 
+                                                      </div>`);
+        j++;
       }
       else if (language[i][j].length === 0){
         keyboardRow.insertAdjacentHTML("beforeend", `<button class="button button_space">${language[i][j]}</button>`);
